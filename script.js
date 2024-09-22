40a677550f6c5a9cb9d735baf80223bbf71680bb
mohamed Ashraf Khalifa <95544850+MohamedKhalifa1@users.noreply.github.com>
2024-09-22 11:26:37 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,5 @@
 const themeToggle = document.getElementById('theme-toggle');
-const currentTheme = localStorage.getItem('theme');
-
-if (currentTheme) {
-  document.body.classList.add(currentTheme);
-}
-
 themeToggle.addEventListener('click', () => {
-  document.body.classList.toggle('dark-theme');
-
-  let theme = 'light';
-  if (document.body.classList.contains('dark-theme')) {
-    theme = 'dark';
-  }
-
-  localStorage.setItem('theme', theme);
+  document.body.classList.toggle('dark-mode');
+  themeToggle.textContent = document.body.classList.contains('dark-mode') ? 'Light Mode' : 'Dark Mode';
 });
